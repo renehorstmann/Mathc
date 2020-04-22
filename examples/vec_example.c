5c0600a8c67dfbeb99d7a45771cf0927a5d8ed7d
renehorstmann <r.horsty@gmail.com>
2020-04-22 14:01:14 +0200

    update vec..

--- examples/vec_example.c
+++ examples/vec_example.c
@@ -15,18 +15,23 @@ void print_veci(const int *vec, int n) {
 }
 
 
+void float_test() {
+    float data_a[3] = {1, 2, 3};
+    float data_b[3] = {10, 20, 30};
+
+    float data_res[3];
+
+    vec3f *a = (vec3f *) data_a;
+    vec3f *b = (vec3f *) data_b;
+    vec3f *res = (vec3f *) data_res;
+
+    *res = vec_add(*a, *b);
+
+    print_vec(data_res, 3);
+}
+
+
 
 int main() {
-
-    int data[3] = {1, 2, 3};
-
-//    Vec3(data).v[0] = 10;
-    print_veci(data, 3);
-
-    double norm = vec3i_norm(Vec3(data));
-    printf("norm: %f\n", norm);
-
-    Vec3(data) = vec_neg(ConstVec3i(data));
-    print_veci(data, 3);
-
+    float_test();
 }
--- include/mathc/typed_generic.h
+++ include/mathc/typed_generic.h
@@ -25,20 +25,14 @@ const int *: ConstVec4i(vec), \
 default: "type_error" \
 )
 
-// todo: const necessary?
+
 #define vec_neg(vec) _Generic((vec), \
 vec3f: vec3f_neg, \
-const vec3f: vec3f_neg, \
 vec4f: vec3f_neg, \
-const vec4f: vec3f_neg, \
 vec3d: vec3d_neg, \
-const vec3d: vec3d_neg, \
 vec4d: vec3d_neg, \
-const vec4d: vec3d_neg, \
 vec3i: vec3i_neg, \
-const vec3i: vec3i_neg, \
 vec4i: vec3i_neg, \
-const vec4i: vec3i_neg, \
 default: "type_error" \
 ) ((vec))
 
@@ -64,20 +58,28 @@ vec4i: vec3i_add_sca, \
 default: "type_error" \
 ) ((vec_a), (scalar_b))
 
-#define vec_add(vec_a, b) _Generic((b), \
-vec3f: vec_add_vec, \
-vec4f: vec_add_vec, \
-vec3d: vec_add_vec, \
-vec4d: vec_add_vec, \
-vec3i: vec_add_vec, \
-vec4i: vec_add_vec, \
-float: vec3f_add_sca, \
-const float: vec3f_add_sca, \
-double: vec3f_add_sca, \
-const double: vec3f_add_sca, \
-int: vec3f_add_sca, \
-const int: vec3f_add_sca, \
-default: "type_error" \
+
+#define vec_add(vec_a, b) _Generic((vec_a), \
+vec3f: _Generic((b), vec3f: vec3f_add_vec, float: vec3f_add_sca, default: "type_error"), \
+vec4f: _Generic((b), vec4f: vec4f_add_vec, float: vec4f_add_sca, default: "type_error"), \
+vec3d: _Generic((b), vec3d: vec3d_add_vec, double: vec3d_add_sca, default: "type_error"), \
+vec4d: _Generic((b), vec3d: vec4d_add_vec, double: vec4d_add_sca, default: "type_error"), \
+vec3i: _Generic((b), vec3i: vec3i_add_vec, int: vec3i_add_sca, default: "type_error"), \
+vec4i: _Generic((b), vec4i: vec4i_add_vec, int: vec4i_add_sca, default: "type_error"), \
+default: "type_error"\
 ) ((vec_a), (b))
+
+
+#define vec_sub_vec(vec_a, scalar_b) _Generic((vec_a), \
+vec3f: vec3f_sub_vec, \
+vec4f: vec4f_sub_vec, \
+vec3d: vec3d_sub_vec, \
+vec4d: vec4d_sub_vec, \
+vec3i: vec3i_sub_vec, \
+vec4i: vec4i_sub_vec, \
+default: "type_error" \
+) ((vec_a), (scalar_b))
+
+// todo: add [sub_sca, div_sca, div_vec]
 
 #endif //MATHC_TYPED_GENERIC_H
