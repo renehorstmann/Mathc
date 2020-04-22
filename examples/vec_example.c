edf4ee82716b790fb4d26fe8749f4af2840c7301
renehorstmann <r.horsty@gmail.com>
2020-04-22 08:55:41 +0200

    new style vec.h init

---
+++ examples/vec_example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "mathc/vec.h"
+
+void print_vec(float *vec, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%f ", vec[i]);
+    puts("");
+}
+
+void print_veci(int *vec, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", vec[i]);
+    puts("");
+}
+
+
+int main() {
+
+
+
+}
