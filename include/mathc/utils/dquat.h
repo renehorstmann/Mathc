c0e3dd2476dc49a90546e34157d75d99ae629455
renehorstmann <r.horsty@gmail.com>
2022-03-17 14:18:54 +0100

    + quat stuff

--- include/mathc/utils/dquat.h
+++ include/mathc/utils/dquat.h
@@ -58,8 +58,8 @@ static dquat dquat_mul_v(const double *q_a, const double *q_b) {
 static dquat dquat_from_angle_axis(dvec4 angle_axis) {
     // from cglm/dquat/glm_dquatv
     double a = angle_axis.w * 0.5f;
-    double c = cos(a);
-    double s = sin(a);
+    double c = dsca_cos(a);
+    double s = dsca_sin(a);
 
     dvec3 axis = dvec3_normalize(angle_axis.xyz);
 
@@ -84,7 +84,7 @@ static dvec4 dquat_to_angle_axis(dquat q) {
      theta          = 2 * atan(sin(theta / 2) / cos(theta / 2))
      */
     double imag_len = dvec3_norm(q.xyz);
-    double angle = 2.0 * atan2f(imag_len, q.w);
+    double angle = 2.0 * dsca_atan2(imag_len, q.w);
 
     dvec4 angle_axis;
     angle_axis.xyz = dvec3_normalize(q.xyz);
@@ -97,9 +97,9 @@ static dvec4 dquat_to_angle_axis_v(const double *q) {
     return dquat_to_angle_axis(DQuat(q));
 }
 
-
+/** returns the orientation of dquat as 3*3 rotation matrix */
 static dmat3 dquat_to_rotation_matrix(dquat q) {
-    // from cglm/dquat/glm_dquat_mat3
+    // from cglm/dquat/glm_dquat_dmat3
     double norm = dvec4_norm(q);
     double s = norm > 0.0 ? 2.0 / norm : 0.0;
 
@@ -136,63 +136,153 @@ static dmat3 dquat_to_rotation_matrix(dquat q) {
     return res;
 }
 
+/** returns the orientation of dquat as 3*3 rotation matrix */
 static dmat3 dquat_to_rotation_matrix_v(const double *q) {
     return dquat_to_rotation_matrix(DQuat(q));
 }
 
 
+/** returns the orientation of a 3*3 rotation matrix as dquat */
 static dquat dquat_from_rotation_matrix(dmat3 mat) {
-    // from cglm/mat3/glm_mat3_dquat
+    // from cglm/dmat3/glm_dmat3_dquat
     dquat res;
     double trace, r, rinv;
     trace = mat.m[0][0] + mat.m[1][1] + mat.m[2][2];
     if (trace >= 0.0) {
-        r = sqrtf(1.0 + trace);
+        r = dsca_sqrt(1.0 + trace);
         rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[1][2] - mat.m[2][1]);
         res.v[1] = rinv * (mat.m[2][0] - mat.m[0][2]);
         res.v[2] = rinv * (mat.m[0][1] - mat.m[1][0]);
-        res.v[3] = r * 0.5;
+        res.v[3] = r * 0.5f;
     } else if (mat.m[0][0] >= mat.m[1][1] && mat.m[0][0] >= mat.m[2][2]) {
-        r = sqrtf(1.0 - mat.m[1][1] - mat.m[2][2] + mat.m[0][0]);
+        r = dsca_sqrt(1.0 - mat.m[1][1] - mat.m[2][2] + mat.m[0][0]);
         rinv = 0.5f / r;
 
-        res.v[0] = r * 0.5;
+        res.v[0] = r * 0.5f;
         res.v[1] = rinv * (mat.m[0][1] + mat.m[1][0]);
         res.v[2] = rinv * (mat.m[0][2] + mat.m[2][0]);
         res.v[3] = rinv * (mat.m[1][2] - mat.m[2][1]);
     } else if (mat.m[1][1] >= mat.m[2][2]) {
-        r = sqrtf(1.0 - mat.m[0][0] - mat.m[2][2] + mat.m[1][1]);
-        rinv = 0.5 / r;
+        r = dsca_sqrt(1.0 - mat.m[0][0] - mat.m[2][2] + mat.m[1][1]);
+        rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[0][1] + mat.m[1][0]);
-        res.v[1] = r * 0.5;
+        res.v[1] = r * 0.5f;
         res.v[2] = rinv * (mat.m[1][2] + mat.m[2][1]);
         res.v[3] = rinv * (mat.m[2][0] - mat.m[0][2]);
     } else {
-        r = sqrtf(1.0 - mat.m[0][0] - mat.m[1][1] + mat.m[2][2]);
-        rinv = 0.5 / r;
+        r = dsca_sqrt(1.0 - mat.m[0][0] - mat.m[1][1] + mat.m[2][2]);
+        rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[0][2] + mat.m[2][0]);
         res.v[1] = rinv * (mat.m[1][2] + mat.m[2][1]);
-        res.v[2] = r * 0.5;
+        res.v[2] = r * 0.5f;
         res.v[3] = rinv * (mat.m[0][1] - mat.m[1][0]);
     }
 
     return res;
 }
 
+/** returns the orientation of a 3*3 rotation matrix as dquat */
 static dquat dquat_from_rotation_matrix_v(const double *mat_3) {
     return dquat_from_rotation_matrix(DMat3(mat_3));
 }
 
+/** returns the orientation of a 3*3 rotation matrix as dquat */
+static void dquat_pose_from_pose_matrix(dvec3 *out_pose_pos, dquat *out_pose_dquat, dmat4 mat_pose) {
+    *out_pose_pos = mat_pose.col[3].xyz;
+    *out_pose_dquat = dquat_from_rotation_matrix(dmat4_get_upper_left3(mat_pose));
+}
 
+/** returns the orientation of a 3*3 rotation matrix as dquat */
+static void dquat_pose_from_pose_matrix_v(double *out_pose_pos3, double *out_pose_dquat, const double *mat_pose44) {
+    dquat_pose_from_pose_matrix((dvec3 *) out_pose_pos3, (dquat *) out_pose_dquat, DMat4(mat_pose44));
+}
+
+/** returns the orientation of a 3*3 rotation matrix as dquat */
+static dmat4 dquat_pose_to_pose_matrix(dvec3 pose_pos, dquat pose_dquat) {
+    dmat4 pose = dmat4_eye();
+    pose.col[3].xyz = pose_pos;
+    dmat4_set_this_upper_left3(&pose, dquat_to_rotation_matrix(pose_dquat));
+    return pose;
+}
+
+/** returns the orientation of a 3*3 rotation matrix as dquat */
+static dmat4 dquat_pose_to_pose_matrix_v(const double *pose_pos3, const double *pose_dquat) {
+    return dquat_pose_to_pose_matrix(DVec3(pose_pos3), DQuat(pose_dquat));
+}
+
+
+/** returns the rotated position pos with the rotation of q */
+static dvec3 dquat_rotate_pos(dquat q, dvec3 pos) {
+    dvec4 p;
+    p.xyz = pos;
+    p.w = 0;
+    // res = q @ pos(w=0) @ conj(q)
+    return dquat_mul(q, dquat_mul(p, dquat_conj(q))).xyz;
+}
+
+/** returns the rotated position pos with the rotation of q */
+static dvec3 dquat_rotate_pos_v(const double *q, const double *pos3) {
+    return dquat_rotate_pos(DQuat(q), DVec3(pos3));
+}
+
+
+/**
+ * Inverts the given pose (position + orientation)
+ * Same as inverting a dmat4 pose
+ */
+static void dquat_pose_inv(dvec3 *out_pose_inv_pos, dquat *out_pose_inv_dquat, dvec3 pose_pos, dquat pose_dquat) {
+    *out_pose_inv_dquat = dquat_inv(pose_dquat);
+    *out_pose_inv_pos = dvec3_neg(dquat_rotate_pos(*out_pose_inv_dquat, pose_pos));
+}
+
+/**
+ * Inverts the given pose (position + orientation)
+ * Same as inverting a dmat4 pose
+ */
+static void dquat_pose_inv_v(double *out_pose_inv_pos3, double *out_pose_inv_dquat,
+                            const double *pose_pos3, const double *pose_dquat) {
+    dquat_pose_inv((dvec3 *) out_pose_inv_pos3, (dquat *) out_pose_inv_dquat, DVec3(pose_pos3), DQuat(pose_dquat));
+}
+
+
+/** returns pos transform by pose a @ b */
+static dvec3 dquat_pose_transform_pos(dvec3 a_pos, dquat a_dquat, dvec3 pos_b) {
+    return dvec3_add_vec(a_pos, dquat_rotate_pos(a_dquat, pos_b));
+}
+
+/** returns the transform of the position pos by the pose of pose_pos and pose_dquat */
+static dvec3 dquat_pose_transform_pos_v(const double *a_pos3, const double *a_dquat, const double *pos3_b) {
+    return dquat_pose_transform_pos(DVec3(a_pos3), DQuat(a_dquat), DVec3(pos3_b));
+}
+
+
+/** returns pose transform of a @ b */
+static void dquat_pose_transform_pose(dvec3 *out_pos, dquat *out_dquat,
+                                     dvec3 a_pos, dquat a_dquat,
+                                     dvec3 b_pos, dquat b_dquat) {
+    *out_pos = dquat_pose_transform_pos(a_pos, a_dquat, b_pos);
+    *out_dquat = dquat_mul(a_dquat, b_dquat);
+}
+
+/** returns pose transform of a @ b */
+static void dquat_pose_transform_pose_v(double *out_pos3, double *out_dquat,
+                                       const double *a_pos3, const double *a_dquat,
+                                       const double *b_pos3, const double *b_dquat) {
+    dquat_pose_transform_pose((dvec3*) out_pos3, (dquat*) out_dquat, 
+                             DVec3(a_pos3), DQuat(a_dquat), DVec3(b_pos3), DQuat(b_dquat));
+}
+
+
+/** spherical linear interpolation between a and b, t in [0 : 1] */
 static dquat dquat_slerp(dquat q_a, dquat q_b, double t) {
-    // q_a cglm/dquat/glm_dquat_slerp
+    // from cglm/dquat/glm_dquat_slerp
     double cos_theta = dvec4_dot(q_a, q_b);
 
-    if (fabs(cos_theta) >= 1.0)
+    if (dsca_abs(cos_theta) >= 1.0)
         return q_a;
 
     if (cos_theta < 0.0) {
@@ -200,21 +290,22 @@ static dquat dquat_slerp(dquat q_a, dquat q_b, double t) {
         cos_theta = -cos_theta;
     }
 
-    double sin_theta = sqrtf(1.0 - cos_theta * cos_theta);
+    double sin_theta = dsca_sqrt(1.0 - cos_theta * cos_theta);
 
     /* LERP q_b avoid zero division */
-    if (fabs(sin_theta) < 0.001)
+    if (dsca_abs(sin_theta) < 0.001)
         return dvec4_mix(q_a, q_b, t);
 
     /* SLERP */
-    double angle = acos(cos_theta);
-    dquat q1 = dvec4_scale(q_a, sin((1.0 - t) * angle));
-    dquat q2 = dvec4_scale(q_b, sin(t * angle));
+    double angle = dsca_acos(cos_theta);
+    dquat q1 = dvec4_scale(q_a, dsca_sin((1.0 - t) * angle));
+    dquat q2 = dvec4_scale(q_b, dsca_sin(t * angle));
 
     q1 = dvec4_add_vec(q1, q2);
     return dvec4_scale(q1, 1.0 / sin_theta);
 }
 
+/** spherical linear interpolation between a and b, t in [0 : 1] */
 static dquat dquat_slerp_v(const double *q_a, const double *q_b, double t) {
     return dquat_slerp(DQuat(q_a), DQuat(q_b), t);
 }
--- include/mathc/utils/quat.h
+++ include/mathc/utils/quat.h
@@ -58,8 +58,8 @@ static quat quat_mul_v(const float *q_a, const float *q_b) {
 static quat quat_from_angle_axis(vec4 angle_axis) {
     // from cglm/quat/glm_quatv
     float a = angle_axis.w * 0.5f;
-    float c = cosf(a);
-    float s = sinf(a);
+    float c = sca_cos(a);
+    float s = sca_sin(a);
 
     vec3 axis = vec3_normalize(angle_axis.xyz);
 
@@ -84,7 +84,7 @@ static vec4 quat_to_angle_axis(quat q) {
      theta          = 2 * atan(sin(theta / 2) / cos(theta / 2))
      */
     float imag_len = vec3_norm(q.xyz);
-    float angle = 2.0f * atan2f(imag_len, q.w);
+    float angle = 2.0f * sca_atan2(imag_len, q.w);
 
     vec4 angle_axis;
     angle_axis.xyz = vec3_normalize(q.xyz);
@@ -97,7 +97,7 @@ static vec4 quat_to_angle_axis_v(const float *q) {
     return quat_to_angle_axis(Quat(q));
 }
 
-
+/** returns the orientation of quat as 3*3 rotation matrix */
 static mat3 quat_to_rotation_matrix(quat q) {
     // from cglm/quat/glm_quat_mat3
     float norm = vec4_norm(q);
@@ -136,18 +136,20 @@ static mat3 quat_to_rotation_matrix(quat q) {
     return res;
 }
 
+/** returns the orientation of quat as 3*3 rotation matrix */
 static mat3 quat_to_rotation_matrix_v(const float *q) {
     return quat_to_rotation_matrix(Quat(q));
 }
 
 
+/** returns the orientation of a 3*3 rotation matrix as quat */
 static quat quat_from_rotation_matrix(mat3 mat) {
     // from cglm/mat3/glm_mat3_quat
     quat res;
     float trace, r, rinv;
     trace = mat.m[0][0] + mat.m[1][1] + mat.m[2][2];
     if (trace >= 0.0f) {
-        r = sqrtf(1.0f + trace);
+        r = sca_sqrt(1.0f + trace);
         rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[1][2] - mat.m[2][1]);
@@ -155,7 +157,7 @@ static quat quat_from_rotation_matrix(mat3 mat) {
         res.v[2] = rinv * (mat.m[0][1] - mat.m[1][0]);
         res.v[3] = r * 0.5f;
     } else if (mat.m[0][0] >= mat.m[1][1] && mat.m[0][0] >= mat.m[2][2]) {
-        r = sqrtf(1.0f - mat.m[1][1] - mat.m[2][2] + mat.m[0][0]);
+        r = sca_sqrt(1.0f - mat.m[1][1] - mat.m[2][2] + mat.m[0][0]);
         rinv = 0.5f / r;
 
         res.v[0] = r * 0.5f;
@@ -163,7 +165,7 @@ static quat quat_from_rotation_matrix(mat3 mat) {
         res.v[2] = rinv * (mat.m[0][2] + mat.m[2][0]);
         res.v[3] = rinv * (mat.m[1][2] - mat.m[2][1]);
     } else if (mat.m[1][1] >= mat.m[2][2]) {
-        r = sqrtf(1.0f - mat.m[0][0] - mat.m[2][2] + mat.m[1][1]);
+        r = sca_sqrt(1.0f - mat.m[0][0] - mat.m[2][2] + mat.m[1][1]);
         rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[0][1] + mat.m[1][0]);
@@ -171,7 +173,7 @@ static quat quat_from_rotation_matrix(mat3 mat) {
         res.v[2] = rinv * (mat.m[1][2] + mat.m[2][1]);
         res.v[3] = rinv * (mat.m[2][0] - mat.m[0][2]);
     } else {
-        r = sqrtf(1.0f - mat.m[0][0] - mat.m[1][1] + mat.m[2][2]);
+        r = sca_sqrt(1.0f - mat.m[0][0] - mat.m[1][1] + mat.m[2][2]);
         rinv = 0.5f / r;
 
         res.v[0] = rinv * (mat.m[0][2] + mat.m[2][0]);
@@ -183,16 +185,104 @@ static quat quat_from_rotation_matrix(mat3 mat) {
     return res;
 }
 
+/** returns the orientation of a 3*3 rotation matrix as quat */
 static quat quat_from_rotation_matrix_v(const float *mat_3) {
     return quat_from_rotation_matrix(Mat3(mat_3));
 }
 
+/** returns the orientation of a 3*3 rotation matrix as quat */
+static void quat_pose_from_pose_matrix(vec3 *out_pose_pos, quat *out_pose_quat, mat4 mat_pose) {
+    *out_pose_pos = mat_pose.col[3].xyz;
+    *out_pose_quat = quat_from_rotation_matrix(mat4_get_upper_left3(mat_pose));
+}
 
+/** returns the orientation of a 3*3 rotation matrix as quat */
+static void quat_pose_from_pose_matrix_v(float *out_pose_pos3, float *out_pose_quat, const float *mat_pose44) {
+    quat_pose_from_pose_matrix((vec3 *) out_pose_pos3, (quat *) out_pose_quat, Mat4(mat_pose44));
+}
+
+/** returns the orientation of a 3*3 rotation matrix as quat */
+static mat4 quat_pose_to_pose_matrix(vec3 pose_pos, quat pose_quat) {
+    mat4 pose = mat4_eye();
+    pose.col[3].xyz = pose_pos;
+    mat4_set_this_upper_left3(&pose, quat_to_rotation_matrix(pose_quat));
+    return pose;
+}
+
+/** returns the orientation of a 3*3 rotation matrix as quat */
+static mat4 quat_pose_to_pose_matrix_v(const float *pose_pos3, const float *pose_quat) {
+    return quat_pose_to_pose_matrix(Vec3(pose_pos3), Quat(pose_quat));
+}
+
+
+/** returns the rotated position pos with the rotation of q */
+static vec3 quat_rotate_pos(quat q, vec3 pos) {
+    vec4 p;
+    p.xyz = pos;
+    p.w = 0;
+    // res = q @ pos(w=0) @ conj(q)
+    return quat_mul(q, quat_mul(p, quat_conj(q))).xyz;
+}
+
+/** returns the rotated position pos with the rotation of q */
+static vec3 quat_rotate_pos_v(const float *q, const float *pos3) {
+    return quat_rotate_pos(Quat(q), Vec3(pos3));
+}
+
+
+/**
+ * Inverts the given pose (position + orientation)
+ * Same as inverting a mat4 pose
+ */
+static void quat_pose_inv(vec3 *out_pose_inv_pos, quat *out_pose_inv_quat, vec3 pose_pos, quat pose_quat) {
+    *out_pose_inv_quat = quat_inv(pose_quat);
+    *out_pose_inv_pos = vec3_neg(quat_rotate_pos(*out_pose_inv_quat, pose_pos));
+}
+
+/**
+ * Inverts the given pose (position + orientation)
+ * Same as inverting a mat4 pose
+ */
+static void quat_pose_inv_v(float *out_pose_inv_pos3, float *out_pose_inv_quat,
+                            const float *pose_pos3, const float *pose_quat) {
+    quat_pose_inv((vec3 *) out_pose_inv_pos3, (quat *) out_pose_inv_quat, Vec3(pose_pos3), Quat(pose_quat));
+}
+
+
+/** returns pos transform by pose a @ b */
+static vec3 quat_pose_transform_pos(vec3 a_pos, quat a_quat, vec3 pos_b) {
+    return vec3_add_vec(a_pos, quat_rotate_pos(a_quat, pos_b));
+}
+
+/** returns the transform of the position pos by the pose of pose_pos and pose_quat */
+static vec3 quat_pose_transform_pos_v(const float *a_pos3, const float *a_quat, const float *pos3_b) {
+    return quat_pose_transform_pos(Vec3(a_pos3), Quat(a_quat), Vec3(pos3_b));
+}
+
+
+/** returns pose transform of a @ b */
+static void quat_pose_transform_pose(vec3 *out_pos, quat *out_quat,
+                                     vec3 a_pos, quat a_quat,
+                                     vec3 b_pos, quat b_quat) {
+    *out_pos = quat_pose_transform_pos(a_pos, a_quat, b_pos);
+    *out_quat = quat_mul(a_quat, b_quat);
+}
+
+/** returns pose transform of a @ b */
+static void quat_pose_transform_pose_v(float *out_pos3, float *out_quat,
+                                       const float *a_pos3, const float *a_quat,
+                                       const float *b_pos3, const float *b_quat) {
+    quat_pose_transform_pose((vec3*) out_pos3, (quat*) out_quat, 
+                             Vec3(a_pos3), Quat(a_quat), Vec3(b_pos3), Quat(b_quat));
+}
+
+
+/** spherical linear interpolation between a and b, t in [0 : 1] */
 static quat quat_slerp(quat q_a, quat q_b, float t) {
     // from cglm/quat/glm_quat_slerp
     float cos_theta = vec4_dot(q_a, q_b);
 
-    if (fabsf(cos_theta) >= 1.0f)
+    if (sca_abs(cos_theta) >= 1.0f)
         return q_a;
 
     if (cos_theta < 0.0f) {
@@ -200,21 +290,22 @@ static quat quat_slerp(quat q_a, quat q_b, float t) {
         cos_theta = -cos_theta;
     }
 
-    float sin_theta = sqrtf(1.0f - cos_theta * cos_theta);
+    float sin_theta = sca_sqrt(1.0f - cos_theta * cos_theta);
 
     /* LERP q_b avoid zero division */
-    if (fabsf(sin_theta) < 0.001f)
+    if (sca_abs(sin_theta) < 0.001f)
         return vec4_mix(q_a, q_b, t);
 
     /* SLERP */
-    float angle = acosf(cos_theta);
-    quat q1 = vec4_scale(q_a, sinf((1.0f - t) * angle));
-    quat q2 = vec4_scale(q_b, sinf(t * angle));
+    float angle = sca_acos(cos_theta);
+    quat q1 = vec4_scale(q_a, sca_sin((1.0f - t) * angle));
+    quat q2 = vec4_scale(q_b, sca_sin(t * angle));
 
     q1 = vec4_add_vec(q1, q2);
     return vec4_scale(q1, 1.0f / sin_theta);
 }
 
+/** spherical linear interpolation between a and b, t in [0 : 1] */
 static quat quat_slerp_v(const float *q_a, const float *q_b, float t) {
     return quat_slerp(Quat(q_a), Quat(q_b), t);
 }
