6e8445ba683387e3862c4178b54cee03d4d02653
renehorstmann <r.horsty@gmail.com>
2022-03-29 13:39:19 +0200

    mathc.h includes all sca types

--- template/in/mathc.h
+++ template/in/mathc.h
@@ -10,4 +10,17 @@
 #include "utils/float.h"
 #include "utils/double.h"
 
+#include "sca/bool.h"
+#include "sca/float.h"
+#include "sca/double.h"
+#include "sca/longdouble.h"
+#include "sca/char.h"
+#include "sca/short.h"
+#include "sca/int.h"
+#include "sca/longlong.h"
+#include "sca/uchar.h"
+#include "sca/ushort.h"
+#include "sca/uint.h"
+#include "sca/ulonglong.h"
+
 #endif //MATHC_MATHC_H
